f034aae6f82b4f27502d62a463c67980d693e155
Bala satish Malladi <balasatishmalladi@gmail.com>
2024-12-28 11:15:54 +0530

    create data structure model for the DB

--- BACKEND/src/controllers/auth.controller.js
+++ BACKEND/src/controllers/auth.controller.js
@@ -1,5 +1,30 @@
-export const signup = (req, res) => {
-  res.send('signup route');
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    // hashing password by using bcrypt
+    if (password.length < 8) {
+      return res.status(400).json({ message: 'Password must contain atleast 8 characters !!' });
+    }
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: 'Email already exists !!' });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+
+
+  } catch (error) {
+
+  }
 };
 
 export const login = (req, res) => {
